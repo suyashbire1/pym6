7227cab724fe5f037ab5d489207f1bd35cfe1986
Suyash Bire <suyash309@gmail.com>
2017-05-03 19:03:29 -0400

    Introduced syntactic sugar for extending slices.

--- variable2.py
+++ variable2.py
@@ -5,19 +5,15 @@ from functools import partial
 class GridNdarray(np.ndarray):
     """A class to hold a grid-located variable."""
     def __new__(cls,input_array,loc):
-        print('new called')
         obj = input_array.view(cls)
         obj.loc = loc
         return obj
 
     def __array_finalize__(self, obj):
-        print('array_finalize called')
         if obj is None: return
         self.loc = getattr(obj, 'loc', None)
-        print(self.loc)
 
     def __array_wrap__(self, out_arr, context=None):
-        print('array_wrap called')
         return np.ndarray.__array_wrap__(self, out_arr, context)
 
 class GridVariable():
@@ -31,16 +27,45 @@ class GridVariable():
             else:
                 self.dom = domain
                 self.loc = loc
-                self.output_loc = kwargs.get('output_loc',self.loc)
-                self._output_slice = self.dom.slices[self.output_loc]
-                self._slice = self._slice_array_to_slice(self._output_slice)
+                self.plot_loc = kwargs.get('plot_loc',self.loc)
+                self._plot_slice = self.dom.slices[self.plot_loc]
+                self.implement_syntactic_sugar_for_plot_slice()
                 break
+
+    @property
+    def plot_slice(self):
+        return self._plot_slice
+
+    @plot_slice.setter
+    def plot_slice(self,operation_string):
+        axis,limit,operation = list(operation_string)
+
+        axes = ['t','l','y','x']
+        limits = ['s','e']
+        operations = { 'm': -1,
+                       'p': +1 }
+        self._plot_slice[axes.index(axis)][limits.index(limit)] += operations[operation]
+
+    def plot_slice_modifier(self,string):
+        self.plot_slice = string
+        return self
+
+    def implement_syntactic_sugar_for_plot_slice(self):
+        axes = ['t','l','y','x']
+        limits = ['s','e']
+        operations = ['m','p']
+        for axis in axes:
+            for limit in limits:
+                for op in operations:
+                    string = axis+limit+op
+                    setattr(self,string,partial(self.plot_slice_modifier,string))
+
 
     @staticmethod
     def extend_halos(array,axis,boundary_index,**kwargs):
         method = kwargs.get('method')
         if method == 'vorticity':
-            slip = kwargs.get('slip')
+            slip = kwargs.get('slip',False)
             slip_multiplyer = 1 if slip else -1
             array_extendor = slip_multiplyer*array.take([boundary_index],axis=axis)
             if boundary_index == 0:
@@ -71,27 +96,29 @@ class GridVariable():
         return array
 
     def read_array(self,extend_kwargs=None,**kwargs):
+        self._slice = self._slice_array_to_slice(self._plot_slice)
         out_array = self._v[self._slice]
-        if self._output_slice[2,0] < 0:
-            out_array = self.extend_halos(out_array,axis=2,
-                                          boundary_index=0,**extend_kwargs)
-        if self._output_slice[2,1] > self.dom.total_ylen:
-            out_array = self.extend_halos(out_array,axis=2,
-                                          boundary_index=-1,**extend_kwargs)
-        if self._output_slice[3,0] < 0:
-            out_array = self.extend_halos(out_array,axis=3,
-                                          boundary_index=0,**extend_kwargs)
-        if self._output_slice[3,1] > self.dom.total_xlen:
-            out_array = self.extend_halos(out_array,axis=3,
-                                          boundary_index=-1,**extend_kwargs)
         if np.ma.isMaskedArray(out_array):
             filled = kwargs.get('filled',np.nan)
             out_array = out_array.filled(filled)
+
+        if self._plot_slice[2,0] < 0:
+            out_array = self.extend_halos(out_array,axis=2,
+                                          boundary_index=0,**extend_kwargs)
+        if self._plot_slice[2,1] > self.dom.total_ylen:
+            out_array = self.extend_halos(out_array,axis=2,
+                                          boundary_index=-1,**extend_kwargs)
+        if self._plot_slice[3,0] < 0:
+            out_array = self.extend_halos(out_array,axis=3,
+                                          boundary_index=0,**extend_kwargs)
+        if self._plot_slice[3,1] > self.dom.total_xlen:
+            out_array = self.extend_halos(out_array,axis=3,
+                                          boundary_index=-1,**extend_kwargs)
         out_array = GridNdarray(out_array,self.loc)
         return out_array
 
     def _modify_slice(self,axis,ns,ne):
-        out_slice = self._output_slice[axis,0] + ns
+        out_slice = self._plot_slice[axis,0] + ns
         out_slice = out_slice[axis,1] + ne
         return out_slice
 
@@ -110,8 +137,8 @@ class GridVariable():
                              v = ['v','v','h','q'],
                              h = ['h','h','v','u'],
                              q = ['q','q','u','v'])
-        out_arr = np.diff(array,1,axis)
-        out_arr.loc = possible_locs[array.loc][axis]
+        out_array = np.diff(array,1,axis)
+        out_array.loc = possible_locs[array.loc][axis]
         return out_arr
 
     @staticmethod
@@ -121,7 +148,8 @@ class GridVariable():
                              h = ['hl','v','u'],
                              q = ['ql','u','v'])
         loc = array.loc
-        average_along_axis = possible_locs[loc].index(new_loc)+1
-        out_array = 0.5*(  np.take(array,:-1,axis=average_along_axis)
-                         + np.take(array,1:,axis=average_along_axis)  )
+        axis = possible_locs[loc].index(new_loc)+1
+        out_array = 0.5*(  np.take(array,range(array.shape[axis]-1),axis=axis)
+                         + np.take(array,range(1,array.shape[axis]),axis=axis)  )
+        out_array.loc = new_loc
         return out_array
