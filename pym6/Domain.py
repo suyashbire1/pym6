82115582c95fb97dd82b41ba106b51982461b801
Suyash Bire <suyash309@gmail.com>
2017-05-08 11:13:25 -0400

    renamed variable2 to Variable and Domain2 to Domain

--- pym6/Domain.py
+++ pym6/Domain.py
@@ -14,8 +14,14 @@ class Grid:
                 setattr(self,var,_fhg.variables[var][:])
             for var in _fhgv.variables:
                 setattr(self,var,_fhgv.variables[var][:])
-        self.R_earth = 6.378e6
+        self.g = 9.8
         self.Rho0 = self.R[-1]
+        b = self.g/self.Rho0*(self.Layer)
+        self.db = (b[0]-b[1])
+        self.R_earth = 6.378e6
+        self.total_xlen = self.lonh.size
+        self.total_ylen = self.lath.size
+        self.total_zlen = self.Layer.size+1
 
     def __repr__(self):
         print('Grid metrics found: {}'.format(vars(self).keys()))
@@ -25,65 +31,51 @@ class Domain(Grid):
     """This class is supposed to be a subset of grid."""
     def __init__(self,geofil,vgeofil,
                       wlon,elon,slat,nlat,
-                      ls,le,ts,te):
+                      **kwargs):
         super(Domain, self).__init__(geofil,vgeofil)
         self.wlon = wlon
         self.elon = elon
         self.slat = slat
         self.nlat = nlat
-        self.ls = ls
-        self.le = le
-        self.ts = ts
-        self.te = te
-        self.populateslices()
+        self.ls = kwargs.get('ls',0)
+        self.le = kwargs.get('le',self.Layer.size+1)
+        self.ts = kwargs.get('ts',0)
+        self.te = kwargs.get('te',None)
+        self.stride_x = kwargs.get('stride_x',1)
+        self.stride_y = kwargs.get('stride_y',1)
+        self.stride_l = kwargs.get('stride_l',1)
+        self.stride_t = kwargs.get('stride_t',1)
+        self.slices = {}
+        self.populate_slices()
 
     @staticmethod
-    def extendslice(sl,dims,n):
-        """Extends the dimension, dim, of slice, sl, by n."""
-        _d = dict(ts = sl[0].start,te = sl[0].stop,
-                  ls = sl[1].start,le = sl[1].stop,
-                  ys = sl[2].start,ye = sl[2].stop,
-                  xs = sl[3].start,xe = sl[3].stop)
-
-        for i, dim in enumerate(dims):
-            _d[dim] = _d[dim]+n[i]
-
-        ts, te = _d['ts'],_d['te']
-        ls, le = _d['ls'],_d['le']
-        ys, ye = _d['ys'],_d['ye']
-        xs, xe = _d['xs'],_d['xe']
-        sl = np.s_[ts:te,ls:le,ys:ye,xs:xe]
-        return sl
-
-    def extendslices(self,sl):
-        """Extends slices in x and y directions. Useful for differentiation."""
-        slices = ['my','py','mx','px','mpy','mpx']
-        exdims = [['ys'],['ye'],['xs'],['xe'],['ys','ye'],['xs','xe']]
-        n = [[-1],[1],[-1],[1],[-1,1],[-1,1]]
-        for i, slc in enumerate(slices):
-            setattr(self,'sl'+slc,self.extendslice(sl,exdims[i],n[i]))
-
-    @staticmethod
-    def findlims(dim,start,end):
+    def find_lims(dim,start,end):
         """Finds the extreme indices of the domain."""
         usefulindx = np.nonzero((dim >= start) & (dim <= end))[0]
         lims = usefulindx[0], usefulindx[-1]+1
         return lims
 
-    def populateslices(self):
+    def populate_slices(self):
         """Creates slices from the wlon,elon,slat,nlat,ls,le,ts,te inputs"""
         ls = self.ls
         le = self.le
         ts = self.ts
         te = self.te
+        stride_x = self.stride_x
+        stride_y = self.stride_y
+        stride_l = self.stride_l
+        stride_t = self.stride_t
         gridloc = ['u','v','h','q']  # Four vertices of an Arakawa C-grid cell
         xloc = ['q','h','h','q']     # X Location on the grid for variable in gridloc
         yloc = ['h','q','h','q']     # Y Location on the grid for variable in gridloc
         for i, loc in enumerate(gridloc):
             x = getattr(self,'lon'+xloc[i])
             y = getattr(self,'lat'+yloc[i])
-            xs, xe = self.findlims(x,self.wlon,self.elon)
-            ys, ye = self.findlims(y,self.slat,self.nlat)
-            sl = np.s_[ts:te, ls:le, ys:ye, xs:xe]
-            setattr(self,'sl',sl)
-            self.extendslices(sl)
+            xs, xe = self.find_lims(x,self.wlon,self.elon)
+            ys, ye = self.find_lims(y,self.slat,self.nlat)
+            slice_array = np.array([[ts,te,stride_t],
+                                    [ls,le,stride_l],
+                                    [ys,ye,stride_y],
+                                    [xs,xe,stride_x]])
+            self.slices[gridloc[i]] = slice_array
+
