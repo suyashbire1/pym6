f8a4341e82605d092065aecba08aa774bc8394d5
Suyash Bire <suyash309@gmail.com>
2017-08-14 04:22:58 -0500

    Introduces the initializer class

--- pym6/Domain.py
+++ pym6/Domain.py
@@ -2,7 +2,33 @@ import numpy as np
 import matplotlib.pyplot as plt
 from netCDF4 import Dataset as dset, MFDataset as mfdset
 
-class Grid:
+class Initializer():
+    """A class to hold names of files, domain limits, etc"""
+
+    def __init__(self,**kwargs):
+        """ geofil:  geometry file
+            vgeofil: vertical geometry file"""
+        self.geofil = kwargs.get('geofil','ocean_geometry.nc')
+        self.vgeofil = kwargs.get('vgeofil','Vertical_coordinate.nc')
+
+        self.fil = kwargs.get('fil')
+        self.fil2 = kwargs.get('fil2')
+        self.fil3 = kwargs.get('fil3')
+        self.wlon = kwargs.get('wlon')
+        self.elon = kwargs.get('elon')
+        self.slat = kwargs.get('slat')
+        self.nlat = kwargs.get('nlat')
+        self.ls = kwargs.get('ls',0)
+        self.le = kwargs.get('le',None)
+        self.ts = kwargs.get('ts',0)
+        self.te = kwargs.get('te',None)
+        self.stride_x = kwargs.get('stride_x',1)
+        self.stride_y = kwargs.get('stride_y',1)
+        self.stride_l = kwargs.get('stride_l',1)
+        self.stride_t = kwargs.get('stride_t',1)
+        self.meanax = kwargs.get('meanax',(0,2))
+
+class Grid():
 
     """ A class to hold grid metrics from MOM6."""
 
@@ -14,6 +40,7 @@ class Grid:
                 setattr(self,var,_fhg.variables[var][:])
             for var in _fhgv.variables:
                 setattr(self,var,_fhgv.variables[var][:])
+
         self.g = 9.8
         self.Rho0 = self.R[-1]
         b = self.g/self.Rho0*(self.Layer)
@@ -27,7 +54,7 @@ class Grid:
         print('Grid metrics found: {}'.format(vars(self).keys()))
         return "MOM6 grid object."
 
-class Domain(Grid):
+class Domain2(Grid):
     """This class is supposed to be a subset of grid."""
     def __init__(self,geofil,vgeofil,
                       wlon,elon,slat,nlat,
@@ -45,6 +72,47 @@ class Domain(Grid):
         self.stride_y = kwargs.get('stride_y',1)
         self.stride_l = kwargs.get('stride_l',1)
         self.stride_t = kwargs.get('stride_t',1)
+        self.slices = {}
+        self.populate_slices()
+
+    @staticmethod
+    def find_lims(dim,start,end):
+        """Finds the extreme indices of the domain."""
+        usefulindx = np.nonzero((dim >= start) & (dim <= end))[0]
+        lims = usefulindx[0], usefulindx[-1]+1
+        return lims
+
+    def populate_slices(self):
+        """Creates slices from the wlon,elon,slat,nlat,ls,le,ts,te inputs"""
+        ls = self.ls
+        le = self.le
+        ts = self.ts
+        te = self.te
+        stride_x = self.stride_x
+        stride_y = self.stride_y
+        stride_l = self.stride_l
+        stride_t = self.stride_t
+        gridloc = ['u','v','h','q']  # Four vertices of an Arakawa C-grid cell
+        xloc = ['q','h','h','q']     # X Location on the grid for variable in gridloc
+        yloc = ['h','q','h','q']     # Y Location on the grid for variable in gridloc
+        for i, loc in enumerate(gridloc):
+            x = getattr(self,'lon'+xloc[i])
+            y = getattr(self,'lat'+yloc[i])
+            xs, xe = self.find_lims(x,self.wlon,self.elon)
+            ys, ye = self.find_lims(y,self.slat,self.nlat)
+            slice_array = np.array([[ts,te,stride_t],
+                                    [ls,le,stride_l],
+                                    [ys,ye,stride_y],
+                                    [xs,xe,stride_x]])
+            self.slices[gridloc[i]] = slice_array
+
+class Domain(Grid):
+    """This class is supposed to be a subset of grid."""
+    def __init__(self,initializer):
+        super(Domain, self).__init__(initializer.geofil,
+                                     initializer.vgeofil)
+        for key,value in initializer.__dict__.items():
+            setattr(self,key,value)
         self.slices = {}
         self.populate_slices()
 
